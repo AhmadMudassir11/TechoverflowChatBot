17aa49e80bc18dff94b21a1e166132671edbd05f
Muaaz Hamood <43746049+Muaaz852@users.noreply.github.com>
2023-02-17 20:08:02 +0500

    Update appContext.js

--- Frontend/src/context/appContext.js
+++ Frontend/src/context/appContext.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "http://localhost:8000";
+const SOCKET_URL = "http://192.46.209.63:84";
 export const socket = io(SOCKET_URL);
 // app context
 export const AppContext = React.createContext();
